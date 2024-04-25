a583e3c61a6b2fd992389a2837d1d2b5401bd214
LoJaxMac10 <maragustin2015@gmail.com>
2024-04-25 18:50:05 -0300

    canvas actualizado
    el canvas se arreglo, funciona bien y se le agrego un Lerp al bajar la vida

--- Assets/Scripts/Canvas & Panels/CanvasManager.cs
+++ Assets/Scripts/Canvas & Panels/CanvasManager.cs
@@ -5,25 +5,40 @@ using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
+    //RECORDAR QUE LOS COLORES SE PONEN CON ALPHA 0, ASI QUE VAN A DESAPARECER.
     public Image barLife;
     public Image stamina;
 
     public Color fullLife, criticalLife;
 
-    // Start is called before the first frame update
-    void Start()
+    public float life2, maxLife2;
+
+    public float lerpLife;
+    public float lastLife;
+
+
+// Start is called before the first frame update
+void Start()
     {
+        lerpLife = maxLife2;
         barLife.color = fullLife;
+    }
+
+    private void Update()
+    {
+        lerpLife = Mathf.Lerp(lerpLife,life2, Time.deltaTime*2);
+        barLife.fillAmount = lerpLife/maxLife2;
     }
 
     public void UpdateMyLife (float life, float maxLife)
     {
-        barLife.fillAmount = life / maxLife;
+        life2 = life;
+        maxLife2 = maxLife;
         
         Debug.Log($" CANVAS: la vida es {life} y la maxima es {maxLife}");
 
         //lerp de color verde a rojo, si quiero que funcione a partir del 50% hago life/(maxlife/2)
-        barLife.color = Color.Lerp(criticalLife, fullLife, life / (maxLife / 2));
+        barLife.color = Color.Lerp(criticalLife, fullLife, life2 / (maxLife2 / 2));
     }
 
     public void UpdateMyStamina()
