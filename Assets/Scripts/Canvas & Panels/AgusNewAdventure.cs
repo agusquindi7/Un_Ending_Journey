787003e26235c9c29b7d884e0e6addac8effcb7a
agusquindi7 <agustinquindimiltraduccion@gmail.com>
2024-04-25 22:16:18 -0300

    Un-Ending Journey pero lindo xd
    nada eso chelo, tiene assets funcionales sin nada blanco, menu funcional, eso xd

---
+++ Assets/Scripts/Canvas & Panels/AgusNewAdventure.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AgusNewAdventure : MonoBehaviour
+{
+    
+    public void NewAdventure()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
